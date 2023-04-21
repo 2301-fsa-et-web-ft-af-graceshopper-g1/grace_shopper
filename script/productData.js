d1d37ef08e4f31ea4076792a8afb8d008d7b96b5
Adhemar Hernandez <adhemarhernandez@gmail.com>
2023-04-21 17:20:48 -0400

    added products api Seed func

---
+++ script/productData.js
@@ -0,0 +1,138 @@
+// Products
+const products = [
+  {
+    name: "HyperX Headset",
+    price: 99.99,
+    imageUrl: "https://m.media-amazon.com/images/I/71MJ3OaVqBL.jpg",
+    description: "A great comfortable headset for an UNBEATABLE price!",
+    stock: 89,
+  },
+  {
+    name: "Logitech Headset",
+    price: 299.99,
+    imageUrl:
+      "https://cdn.shopify.com/s/files/1/0252/9705/9876/products/5_1_dcb1c1c1-6d88-420e-bff3-32eb5ef35077_1200x.png?v=1631134068",
+    description: "A great comfortable headset for a actual BEATABLE price!",
+    stock: 166,
+  },
+  {
+    name: "SteelSeries Keyboard",
+    price: 129.99,
+    imageUrl:
+      "https://media.steelseriescdn.com/thumbs/catalog/items/64847/a0bc32930344430a86be030026292d14.png.350x280_q100_crop-fit_optimize.png",
+    description: "Newest KeyBoard from steelseries. the BEST GAMING KEYBOARD!",
+    stock: 221,
+  },
+  {
+    name: "Strange Gaming Mouse",
+    price: 300.0,
+    imageUrl:
+      "https://assets.hongkiat.com/uploads/creative-unusual-mice/man_body.jpg?newedit",
+    description:
+      "A strange mouse from a even stranger land. seems to resemble the physique of a human male.",
+    stock: 236,
+  },
+  {
+    name: "Optical Ferrari Car Mouse",
+    price: 500.0,
+    imageUrl:
+      "https://assets.hongkiat.com/uploads/creative-unusual-mice/ferrari_mouse.jpg?newedit",
+    description:
+      "Car 3D optical mouse looks like real Ferrari sports car that will appeal to all racing fans. LED lights in car running lights looks fancy.",
+    stock: 538,
+  },
+  {
+    name: "MSI GAMING LAPTOP",
+    price: 1999.99,
+    imageUrl:
+      "https://c1.neweggimages.com/ProductImageCompressAll1280/34-156-395-36.jpg",
+    description:
+      "#1 BEST SELLER - The all-new Vector GP series brings a new magnitude of performance. With up to the latest 12th Gen Intel® Core™ i9 processor, NVIDIA® GeForce RTX™ 30 series graphics, and QHD display, re-imagine how you perform!",
+    stock: 382,
+  }, 
+  {
+    name: "ASUS ROG Strix Scar 15 (2022) Gaming Laptop",
+    price: 1914.56,
+    imageUrl: "https://c1.neweggimages.com/ProductImageCompressAll1280/34-236-100-V19.jpg",
+    description: "Great Gaming laptop that includes 300Hz IPS FHD Display, NVIDIA GeForce RTX 3060, Intel Core i9 12900H, 16GB DDR5, 512GB SSD, Per-Key RGB Keyboard, Windows 11 Home, G533ZM-ES93",
+    stock: 62
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } , {
+    name:
+    price:
+    imageUrl:
+    description:
+    stock:
+  } ,
+];
